a2def7f8d3f3891ee36bd44866c408313bc6575f
aluno <aluno@ifpe>
2025-04-16 21:17:58 -0300

    create produto/entregador

---
+++ src/main/java/br/com/ifpe/oxefood/modelo/entregador/Entregador.java
@@ -0,0 +1,74 @@
+package br.com.ifpe.oxefood.modelo.entregador;
+
+import java.time.LocalDate;
+import org.hibernate.annotations.SQLRestriction;
+
+import br.com.ifpe.oxefood.util.entity.EntidadeAuditavel;
+
+//import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "Entregador")
+@SQLRestriction("habilitado = true")// acrescente em todas as consultas uma clausula where: habilitado = true
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Entregador extends EntidadeAuditavel  {
+  
+   //@Column
+   private String nome;
+    
+   //@Column
+   private LocalDate dataNascimento;
+   
+   //@Column
+   private String cpf;
+   
+   //@Column
+   private String foneCelular;
+  
+   //@Column
+   private String foneFixo;
+
+   private Integer qtdEntregasRealizadas;
+
+   private Double valorFrete;
+
+   private String enderecoRua;
+
+   private String enderecoComplemento;
+
+   private String enderecoNumero;
+
+   private String  enderecoBairro;
+   
+   private String  enderecoCidade;
+
+   private String enderecoCep ; 
+
+   private String enderecoUf;
+   
+   private Boolean ativo;
+
+}
+
+
+
+
+
+
+
+
+
--- src/test/java/br/com/ifpe/oxefood/OxefoodFernandaApplicationTests.java
+++ src/test/java/br/com/ifpe/oxefood/OxefoodFernandaApplicationTests.java
@@ -3,7 +3,9 @@ package br.com.ifpe.oxefood;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
+
 class OxefoodFernandaApplicationTests {
 
 	@Test
