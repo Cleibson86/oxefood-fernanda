6eae9fc1499c01eabfd12febb96ebddd79ea93bd
aluno <aluno@ifpe>
2025-04-23 21:22:05 -0300

    implementação/cliente/produto/entregador

--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
@@ -1,9 +1,12 @@
 package br.com.ifpe.oxefood.modelo.cliente;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
+
 
 @Service
 public class ClienteService {
@@ -16,6 +19,15 @@ public class ClienteService {
 
         cliente.setHabilitado(Boolean.TRUE);
         return repository.save(cliente);
+    }
+    public List<Cliente> listarTodos() {
+  
+        return repository.findAll();//SELECT * FROM Cliente
+    }
+
+    public Cliente obterPorID(Long id) {
+
+        return repository.findById(id).get();// SELECT * Cliente where id
     }
 
 }
